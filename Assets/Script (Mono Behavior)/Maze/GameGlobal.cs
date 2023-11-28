3ca169f48dd61495db966ac4aa6403d11dabf740
baotuannguyen16 <110767681+baotuannguyen16@users.noreply.github.com>
2023-11-28 09:33:49 +0700

    Login and Register

---
+++ Assets/Script (Mono Behavior)/Maze/GameGlobal.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class GameGlobal
+{
+    public static int level = 0;
+
+    public static void Play(int lv)
+    {
+        level = lv;
+        Restart();
+    }
+
+    public static void Restart()
+    {
+        //SceneManager.LoadScene("Gameplay(Maze)");
+    }
+}
